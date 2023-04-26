4cbd81ddec5b11bb3e9b4c4f915a33b9b71d98f1
Antoine Kevin CHOULA <76167680+kevAnto@users.noreply.github.com>
2023-04-26 18:20:45 +0100

    workflow edited

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
+return "<h1>MasterClass. Version : 00  </h1>";
 }
 }
